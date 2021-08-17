fd86df0f1cc17c84dc467a3e9b057401c59fd9b0
Elliop <abdelmouhayminegx@gmail.com>
2021-08-18 00:56:05 +0100

    First Commit

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Tic Tac Toe</title>
+      </Head>
+      <main>Tic Tac Toe!</main>
+    </div>
+  );
+}
